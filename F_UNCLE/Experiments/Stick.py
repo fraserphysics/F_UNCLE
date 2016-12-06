Update to Isentrope to allow correct CJ calculation

F_UNCLE/Experiments/Stick.py
@@ -298,7 +298,7 @@ class Stick(GausianExperiment):
 
             # v_eos = np.logspace(np.log10(v_min), np.log10(v_max), 30)
             v_eos = np.linspace(v_min, v_max, 30)
-            ax1.plot(v_eos, rayl_line(vel_cj, v_eos, eos, v_0), linestyles[1],
+            ax1.plot(v_eos, rayl_line(vel_cj, v_eos, eos, v_0, 0.0), linestyles[1],
                      label=labels[1])
             ax1.plot(vol_cj, p_cj, linestyles[2], label=labels[2])
             ax1.plot(v_0, 0.0, linestyles[3], label=labels[3])

F_UNCLE/Utils/Experiment.py
@@ -285,6 +285,14 @@ class Experiment(Struc):
 
         return NotImplemented
 
+    def _get_resp(new_dofi, exp, model_dct, mkey, init_dat):
+        """Class method used in the parallel map function
+        """
+        model_dct[mkey] = model_dct[mkey].update_dof(new_dofi)                
+        return -exp.compare(init_dat[0], init_dat[1][0],
+                            exp(model_dct))
+    # end
+    
     def get_sens_pll(self, models, model_key, initial_data=None):
         """Parallel evaluation of each model's sensitivities
 
@@ -298,6 +306,8 @@ class Experiment(Struc):
         """
 
         import concurrent.futures
+
+ #       import pdb
         
         models = copy.deepcopy(models)
         model = models[model_key]
@@ -312,46 +322,61 @@ class Experiment(Struc):
                              model.shape()))
         inp_mat = np.zeros((model.shape(),
                             model.shape()))
+        new_dof_mat = []
         new_dofs = np.array(copy.deepcopy(model.get_dof()),
                             dtype=np.float64)
 
-        def get_resp(new_dofs):
-            """
-            """
-            models[model_key] = model.update_dof(new_dofs)                
-            return -self.compare(initial_data[0], initial_data[1][0],
-                                 self(models))
+        for i, coeff in enumerate(model.get_dof()):
+            new_dofs[i] += float(coeff * step_frac)
+            inp_mat[:, i] = (new_dofs - model.get_dof())
+            new_dof_mat.append(copy.deepcopy(new_dofs))
+            new_dofs[i] -= float(coeff * step_frac)
         # end
             
-        # end 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
-            futures = {}
-            for i, coeff in enumerate(model.get_dof()):
-                print(i)
-                new_dofs[i] += float(coeff * step_frac)
-                inp_mat[:, i] = (new_dofs - model.get_dof())
-                futures[executor.submit(
-                    get_resp,
-                    new_dofs)] = i
-
-                new_dofs[i] -= float(coeff * step_frac)
-            # end
-
-            for ftr in concurrent.futures.as_completed(futures):
-                i = futures[ftr]
-
-                try:
-                    resp = ftr.result()
-                except Exception as exc:
-                    raise RuntimeError('{:s} sensitivities generated an'
-                                       ' exception {:s}'.
-                                       format(self.get_inform(1), exc))
-                else:
-                    resp_mat[:, i] = resp
-                # end
+        with concurrent.futures.ProcessPoolExecutor(max_workers=12) as executor:
+            for i, resp in enumerate(executor.map(
+                    Experiment._get_resp,
+                    new_dof_mat,
+                    [copy.deepcopy(self) for i in range(len(new_dof_mat))],
+                    [copy.deepcopy(models) for i in range(len(new_dof_mat))],
+                    [copy.deepcopy(model_key) for i in range(len(new_dof_mat))],
+                    [copy.deepcopy(initial_data) for i in range(len(new_dof_mat))],
+                    chunksize=4)):
+                resp_mat[:, i] = resp
             # end                
         # end
         
+        # with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+        #     futures = {}
+        #     for i, coeff in enumerate(model.get_dof()):
+        #         new_dofs[i] += float(coeff * step_frac)
+        #         inp_mat[:, i] = (new_dofs - model.get_dof())
+        #         futures[executor.submit(
+        #             Experiment._get_resp,
+        #             new_dofs,
+        #             copy.deepcopy(self),
+        #             copy.deepcopy(models),
+        #             copy.deepcopy(model_key),
+        #             copy.deepcopy(initial_data))] = i
+
+        #         new_dofs[i] -= float(coeff * step_frac)
+        #     # end
+
+        #     for ftr in concurrent.futures.as_completed(futures):
+        #         i = futures[ftr]
+
+        #         try:
+        #             resp = ftr.result()
+        #         except Exception as exc:
+        #             raise RuntimeError('{:s} sensitivities generated an'
+        #                                ' exception {:s}'.
+        #                                format(self.get_inform(1), exc))
+        #         else:
+        #             resp_mat[:, i] = resp
+        #         # end
+        #     # end
+        # # end
+ 
         sens_matrix = np.linalg.lstsq(inp_mat, resp_mat.T)[0].T
         return np.where(np.fabs(sens_matrix) > 1E-21,
                         sens_matrix,